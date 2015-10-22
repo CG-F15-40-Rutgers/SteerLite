949fec23f31fce777ce8959ae26d39e26ccab71a
Maxine <maxine.deines@gmail.com>
2015-10-22 19:31:06 -0400

    initial minkowski difference function

--- steerlib/src/GJK_EPA.cpp
+++ steerlib/src/GJK_EPA.cpp
@@ -12,6 +12,15 @@ SteerLib::GJK_EPA::GJK_EPA()
 {
 }
 
+std::vector<Util::Vector> minkowskiDiff(const std::vector<Util::Vector>& _shapeA, const std::vector<Util::Vector>& _shapeB) {
+	std::vector<Util::Vector> diffPoints = std::vector<Util::Vector>();
+	for each(Util::Vector v in _shapeA) {
+		for each(Util::Vector w in _shapeB) {
+			diffPoints.push_back(v - w);
+		}
+	}
+}
+
 //Look at the GJK_EPA.h header file for documentation and instructions
 bool SteerLib::GJK_EPA::intersect(float& return_penetration_depth, Util::Vector& return_penetration_vector, const std::vector<Util::Vector>& _shapeA, const std::vector<Util::Vector>& _shapeB)
 {
