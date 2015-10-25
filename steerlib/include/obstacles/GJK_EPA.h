e07a5220154f2e0296774904bb2e835459ce3684
RoVernekar <ro.vernekar@gmail.com>
2015-10-24 23:28:44 -0400

    Added GJK

--- steerlib/include/obstacles/GJK_EPA.h
+++ steerlib/include/obstacles/GJK_EPA.h
@@ -21,7 +21,7 @@ namespace SteerLib
     class STEERLIB_API GJK_EPA
     {
         public:
-			GJK_EPA(std::vector<Util::Vector>&, bool&);
+			GJK_EPA();
 
             /*
              *
@@ -129,8 +129,10 @@ namespace SteerLib
             static bool intersect(float& return_penetration_depth, Util::Vector& return_penetration_vector, const std::vector<Util::Vector>& _shapeA, const std::vector<Util::Vector>& _shapeB);
 
         private:
-			Util::Vector getFarthestPoint(std::vector<Util::Vector>&, Util::Vector&);
-			Util::Vector getSupport(std::vector<Util::Vector>&, std::vector<Util::Vector>&, Util::Vector&);
+			static Util::Vector getFarthestPoint(const std::vector<Util::Vector>&, Util::Vector&);
+			static Util::Vector getSupport(const std::vector<Util::Vector>& _shapeA, const std::vector<Util::Vector>& _shapeB, Util::Vector&);
+			static bool containsOrigin(std::vector<Util::Vector>&, Util::Vector&);
+			static void GJK(std::vector<Util::Vector>&, bool&, const std::vector<Util::Vector>& _shapeA, const std::vector<Util::Vector>& _shapeB);
 
     }; // class GJK_EPA
 
--- steerlib/src/GJK_EPA.cpp
+++ steerlib/src/GJK_EPA.cpp
@@ -7,8 +7,12 @@
 
 #include "obstacles/GJK_EPA.h"
 
-// Returns the farthest point in shape in direction d
-Util::Vector SteerLib::GJK_EPA::getFarthestPoint(std::vector<Util::Vector>& shape, Util::Vector& d)
+SteerLib::GJK_EPA::GJK_EPA()
+{
+}
+
+// Returns farthest point in shape in direction d
+Util::Vector SteerLib::GJK_EPA::getFarthestPoint(const std::vector<Util::Vector>& shape, Util::Vector& d)
 {
 	float highest = -FLT_MAX;
 	Util::Vector farthestPoint = Util::Vector(0, 0, 0);
@@ -16,7 +20,7 @@ Util::Vector SteerLib::GJK_EPA::getFarthestPoint(std::vector<Util::Vector>& shap
 	for (int i = 0; i < shape.size(); i++)
 	{
 		Util::Vector v = shape[i];
-		float dot = v.x * d.x + v.y * d.y;
+		float dot = v * d;
 
 		if (dot > highest)
 		{
@@ -29,17 +33,128 @@ Util::Vector SteerLib::GJK_EPA::getFarthestPoint(std::vector<Util::Vector>& shap
 }
 
 // Returns farthest point in Minkowski Difference between shapeA and shapeB in direction d
-Util::Vector SteerLib::GJK_EPA::getSupport(std::vector<Util::Vector>& shapeA, std::vector<Util::Vector>& shapeB, Util::Vector& d)
+Util::Vector SteerLib::GJK_EPA::getSupport(const std::vector<Util::Vector>& _shapeA, const std::vector<Util::Vector>& _shapeB, Util::Vector& d)
 {
-	return getFarthestPoint(shapeA, d) - getFarthestPoint(shapeB, -d);
+	return getFarthestPoint(_shapeA, d) - getFarthestPoint(_shapeB, -d);
 }
 
-SteerLib::GJK_EPA::GJK_EPA(std::vector<Util::Vector>& simplex, bool& is_colliding)
+// Returns true if simplex contains the origin
+// Modifies d to new direction to check
+bool SteerLib::GJK_EPA::containsOrigin(std::vector<Util::Vector>& simplex, Util::Vector& d)
 {
+	if (simplex.size() == 2)
+	{
+		// Simplex is a line segment
+		Util::Vector a = simplex[1], b = simplex[0];
+
+		// Get vector ab
+		Util::Vector ab = b - a;
+
+		// Calculate vector perpendicular to ab
+		Util::Vector abPerp = Util::cross(Util::cross(ab, -a), ab);
+
+		// Set new direction perpendicular to ab
+		d = abPerp;
+	}
+	else if (simplex.size() == 3)
+	{
+		// Simplex is a triangle
+		Util::Vector a = simplex[2], b = simplex[1], c = simplex[0];
+
+		// Get vector ab and ac
+		Util::Vector ab = b - a;
+		Util::Vector ac = c - a;
+
+		// Calculate vectors perpendicular to ab and ac
+		Util::Vector abPerp = Util::cross(Util::cross(ac, ab), ab);
+		Util::Vector acPerp = Util::cross(Util::cross(ab, ac), ac);
+
+		if (abPerp * -a > 0)
+		{
+			// Remove c from simplex
+			simplex.erase(simplex.begin());
+
+			// Set new direction perpendicular to ab
+			d = abPerp;
+		}
+		else if (acPerp * -a > 0)
+		{
+			// Remove b from simplex
+			simplex.erase(simplex.begin() + 1);
+
+			// Set new direction perpendicular to ac
+			d = acPerp;
+		}
+		else
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+// Uses GJK to check for collision between _shapeA and _shapeB
+// Sets simplex shape if there is a collision
+// Sets is_colliding to true if there is a collision
+void SteerLib::GJK_EPA::GJK(std::vector<Util::Vector>& simplex, bool& is_colliding, const std::vector<Util::Vector>& _shapeA, const std::vector<Util::Vector>& _shapeB)
+{
+	// Pick a direction to start in
+	Util::Vector d = Util::Vector(1, 0, 0);
+
+	// Add first point to simplex
+	Util::Vector s = getSupport(_shapeA, _shapeB, d);
+	simplex.push_back(s);
+
+	// Switch to opposite direction for next point
+	d = -d;
+
+	while (true)
+	{
+		// Add new point to simplex
+		Util::Vector a = getSupport(_shapeA, _shapeB, d);
+
+		float dot = a * d;
+		if (dot < 0)
+		{
+			// The new point doesn't pass the origin
+			simplex.clear();
+			is_colliding = false;
+			break;
+		}
+		else
+		{
+			simplex.push_back(a);
+
+			if (containsOrigin(simplex, d))
+			{
+				// Simplex contains origin
+				is_colliding = true;
+				break;
+			}
+		}
+	}
 }
 
 //Look at the GJK_EPA.h header file for documentation and instructions
 bool SteerLib::GJK_EPA::intersect(float& return_penetration_depth, Util::Vector& return_penetration_vector, const std::vector<Util::Vector>& _shapeA, const std::vector<Util::Vector>& _shapeB)
 {
-    return false; // There is no collision
+	std::vector<Util::Vector> simplex;
+	bool is_colliding;
+
+	GJK(simplex, is_colliding, _shapeA, _shapeB);
+
+	if (is_colliding)
+	{
+		//return_penetration_depth = ;
+		//return_penetration_vector = ;
+		return true;
+	}
+	else
+	{
+		return_penetration_depth = 0;
+		return_penetration_vector.zero();
+		return false;
+	}
+
 }
