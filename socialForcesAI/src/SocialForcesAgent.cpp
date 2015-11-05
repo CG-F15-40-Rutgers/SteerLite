be713d9891ce098f2ec6db6208030592f92f3661
Charu Jain <charujain017@gmail.com>
2015-11-04 20:02:56 -0500

    wall repulsion

--- socialForcesAI/src/SocialForcesAgent.cpp
+++ socialForcesAI/src/SocialForcesAgent.cpp
@@ -269,9 +269,47 @@ Util::Vector SocialForcesAgent::calcAgentRepulsionForce(float dt)
 
 Util::Vector SocialForcesAgent::calcWallRepulsionForce(float dt)
 {
-    std::cerr<<"<<<calcWallRepulsionForce>>> Please Implement my body\n";
+	std::cerr << "<<<calcWallRepulsionForce>>> Please Implement my body\n";
 
-    return Util::Vector(0,0,0);
+	Util::Vector wall_repulsion_force = Util::Vector(0, 0, 0);
+	SteerLib::ObstacleInterface * tmp_ob;
+
+	std::set<SteerLib::SpatialDatabaseItemPtr> _neighbors;
+	gEngine->getSpatialDatabase()->getItemsInRange(_neighbors,
+		_position.x - (this->_radius + _SocialForcesParams.sf_query_radius),
+		_position.x + (this->_radius + _SocialForcesParams.sf_query_radius),
+		_position.z - (this->_radius + _SocialForcesParams.sf_query_radius),
+		_position.z + (this->_radius + _SocialForcesParams.sf_query_radius),
+		dynamic_cast<SteerLib::SpatialDatabaseItemPtr>(this));
+
+	for (std::set<SteerLib::SpatialDatabaseItemPtr>::iterator neighbour = _neighbors.begin(); neighbour != _neighbors.end(); neighbour++)
+	{
+		if (!(*neighbour)->isAgent())
+		{
+			tmp_ob = dynamic_cast<SteerLib::ObstacleInterface *>(*neighbour);
+		}
+		else
+		{
+			continue;
+		}
+		if (tmp_ob->computePenetration(this->position(), this->radius()) > 0.000001)
+		{
+			Util::Vector wall_normal = calcWallNormal(tmp_ob);
+			std::pair<Util::Point, Util::Point> line = calcWallPointsFromNormal(tmp_ob, wall_normal);
+
+			std::pair<float, Util::Point> min_stuff = minimum_distance(line.first, line.second, position());
+
+			wall_repulsion_force = wall_normal * (min_stuff.first + radius()) *
+				_SocialForcesParams.sf_body_force;
+
+		}
+		else
+		{
+			Util::Vector wall_normal = calcWallNormal(tmp_ob(position));
+		}
+	}
+		return wall_repulsion_force;
+	
 }
 
 
